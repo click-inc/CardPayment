741496dd7516e62435ea3b4c05220ebb70a460e5
Mahe077 <2019cs077@stu.ucsc.lk>
2022-10-28 17:52:07 +0530

    Card payment service

---
+++ src/routers/card.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const Auth = require("../middleware/auth");
+
+const router = new express.Router();
+
+//signup
+router.post("/card", Auth, async (req, res) => {
+  try {
+    res.status(201).send({ status: "success" });
+  } catch (error) {
+    res.status(400).send({ status: "failed" });
+  }
+});
+
+module.exports = router;
